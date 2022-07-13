a17ea0ac937d1ea63e4ada67ae2c71fc9aa11df1
Mihir Narkar <Mihirnarkar19@gmail.com>
2022-07-14 00:54:18 +0530

    new

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
+const { body, validationResult } = require('express-validator')
 
 
 // Create a user using: POST "/api/auth/"
-router.post('/',(req,res)=>{
-    console.log(req.body)
-    const user = User(req.body)
-    user.save()
+router.post('/',[
+    body('name',"Minimum length of name should be 3").isLength({ min: 3 }),
+    body('email',"Enter valid email/unique email").isEmail(),
+    body('password',"Minimum length of password should be 3").isLength({ min: 3 }),
+],(req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
     res.send(req.body)
 })
 
